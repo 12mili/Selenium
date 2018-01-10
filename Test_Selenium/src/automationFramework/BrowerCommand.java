0fcf74bee525b6a012674730dcf2f8a0f1b9c930
12mili <v.mili88@gmail.com>
2018-01-09 21:44:01 -0700

    Rechanged  close to quit Rechanged Amazon to Walmart

--- Test_Selenium/src/automationFramework/BrowerCommand.java
+++ Test_Selenium/src/automationFramework/BrowerCommand.java
@@ -7,13 +7,13 @@ public class BrowerCommand {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Basant\\Downloads\\chromedriver_win32\\chromedriver.exe" );
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.com");
+		driver.get("https://www.walmart.com");
 		driver.manage().window().maximize();
 		String bob = driver.getTitle();
 		System.out.println(bob);
 		 String camp = driver.getCurrentUrl();
 		 System.out.println(camp);
-		 driver.close();
+		 driver.quit();
 		
 	   
 	
