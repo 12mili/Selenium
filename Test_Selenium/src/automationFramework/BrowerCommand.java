94b3760247a62bf722957d31b1b2ca7d705d7dda
12mili <v.mili88@gmail.com>
2018-01-09 21:56:56 -0700

    Rechanged amazon to Sling

--- Test_Selenium/src/automationFramework/BrowerCommand.java
+++ Test_Selenium/src/automationFramework/BrowerCommand.java
@@ -7,7 +7,7 @@ public class BrowerCommand {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Basant\\Downloads\\chromedriver_win32\\chromedriver.exe" );
 		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.com");
+		driver.get("https://www.sling.com");
 		driver.manage().window().maximize();
 		String bob = driver.getTitle();
 		System.out.println(bob);
